822f1996022920a1318096fa4059160c912a31a1
Use your GitHub username <Enter your email address>
2020-05-14 11:26:08 -0600

    css and java

---
+++ lesson4/js/ham.js
@@ -0,0 +1,6 @@
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigate')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+/*code from responsive 'hamburger menu' WDD 230 */
