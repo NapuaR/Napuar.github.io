8c1ba523c13430f752d4bbc38df6691c347537da
Use your GitHub username <Enter your email address>
2020-05-21 10:20:07 -0600

    updates to html

--- lesson5/js/ham.js
+++ lesson5/js/ham.js
@@ -1,4 +1,5 @@
-function toggleMenu(){
-    document.getElementById("primaryNav").classList.toggle("hide");
-    }
-       
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+/*resposive navigation, a menu and opens and closes on click, from WDD 230:*/
